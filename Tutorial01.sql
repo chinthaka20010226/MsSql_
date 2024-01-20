4f68b7e7c6a2e6b58daa8051412b4f19fb045187
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-20 16:55:19 +0530

    define the primary key -> method 02

--- Tutorial01.sql
+++ Tutorial01.sql
@@ -3,9 +3,10 @@ Create Database Tutorial01
 Use Tutorial01
 
 Create Table Client(
-ClientNo varchar(6) primary key,
+ClientNo varchar(6),
 Name varchar(20),
 City varchar(20),
 Date_Joined DateTime,
-Balance_Due money
+Balance_Due money,
+Constraint client_pk primary key(ClientNo)
 )
