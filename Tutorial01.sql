5e70f3ad4d8227fad55f2433147df623acfa3dfe
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-20 16:59:41 +0530

    identity(1,1) is automatically increment One by One, but primary key has data type must int...

--- Tutorial01.sql
+++ Tutorial01.sql
@@ -3,10 +3,9 @@ Create Database Tutorial01
 Use Tutorial01
 
 Create Table Client(
-ClientNo varchar(6),
+ClientNo int primary key identity(1,1),
 Name varchar(20),
 City varchar(20),
 Date_Joined DateTime,
-Balance_Due money,
-Constraint client_pk primary key(ClientNo)
+Balance_Due money
 )
